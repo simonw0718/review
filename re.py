f64480d5e85d30c12d4645bc6c7a8fc3eadb8b60
Simon Wang <simonw0718@gmail.com>
2019-06-26 22:22:42 +0800

    2nd ver.

--- re.py
+++ re.py
@@ -1,7 +1,11 @@
 data = []
+count = 0
 with open('reviews.txt', 'r') as f:
     for review in f:
         data.append(review.strip())
+        count += 1 #快寫法 c = c+1
+        if count % 1000 == 0:
+        	print(len(data))
 print(data[0])
 print(len(data))
 
