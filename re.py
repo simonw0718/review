e331c23301f678fdf102fb13882b5c776e0a97b8
Simon Wang <simonw0718@gmail.com>
2019-06-26 22:30:55 +0800

    3rd ver.

--- re.py
+++ re.py
@@ -4,8 +4,16 @@ with open('reviews.txt', 'r') as f:
     for review in f:
         data.append(review.strip())
         count += 1 #快寫法 c = c+1
-        if count % 1000 == 0:
+        if count % 10000 == 0:
         	print(len(data))
 print(data[0])
-print(len(data))
+total = len(data)
+print(len(data[0]))
 
+length = 0
+for comt in data:
+	length += len(comt)
+
+print(length)
+average = length / total
+print(average)
