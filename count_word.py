8aef8d2901f867e8b426dd8890efeb8aaea29db0
Simon Wang <simonw0718@gmail.com>
2019-07-10 20:35:14 +0800

    add time counter

--- count_word.py
+++ count_word.py
@@ -1,3 +1,6 @@
+import time
+
+
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
@@ -9,6 +12,9 @@ with open('reviews.txt', 'r') as f:
 print('讀完了,共有', len(data), '個資料')
 print(data[0])
 
+
+#文字計數
+start_time = time.time() #使用time function紀錄系統時間
 wc ={} # word_count
 for d in data:
     words = d.split() # split預設就是空白建
@@ -20,15 +26,21 @@ for d in data:
 for word in wc:
     if wc[word] > 100000:
         print(word,wc[word])
+
+end_time = time.time() #紀錄當時時間
+
+print('花了', end_time - start_time, 'seconds')
 print(len(wc))
 print(wc['Simon'])
 
+
+#查找文字
 while True:
     word = input('想查啥')
     if word == 'q':
         break
     elif word not in wc:
-    	print('沒有這個字')
+        print('沒有這個字')
     else:
-    print(word, '出現過的次數為', wc[word])    
+        print(word, '出現過的次數為', wc[word])    
 print('感謝使用')
